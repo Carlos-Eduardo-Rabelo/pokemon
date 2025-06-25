2c5eaa15ed85ac36950f59e827ff8e9ca9d5c082
Carlos-Eduardo-Rabelo <caduvera220@gmail.com>
2025-06-25 00:03:14 -0300

    Pequeno ajuste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return(
     <div className=" h-[60vh] bg-white content-center">
-      <div className="m-auto w-100 h-100 bg-red-300 rounded-xl">
-        <h1 className="p-5 m-auto w-100 h-30 text-center text-2xl content-center font-bold bg-red-400 rounded-t-xl">
+      <div className="m-auto w-70 md:w-100 h-100 bg-red-300 rounded-xl">
+        <h1 className="p-5 m-auto w-70 md:w-100 h-30 text-center text-2xl content-center font-bold bg-red-400 rounded-t-xl">
           Bem-vindo ao Meu Poke!
         </h1>
-        <h2 className="p-5 m-auto h-40 w-100 text-xl text-center content-center">
+        <h2 className="p-5 m-auto h-40 w-70 md:w-100 text-xl text-center content-center">
           Aqui você pode encontrar informações sobre os Pokémons e suas habilidades.
         </h2>
         <div className="m-auto h-30 content-center">
