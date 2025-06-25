ba98cc03ff8f1926127db1a538d2c51d5a90537d
Carlos-Eduardo-Rabelo <caduvera220@gmail.com>
2025-06-24 23:32:15 -0300

    Adicionando página Giro e Pokemons do Usuario

---
+++ src/app/giros/page.tsx
@@ -0,0 +1,12 @@
+export default function Giros() {
+    return(
+    <div className=" h-[60vh] bg-white content-center">
+      <div className="m-auto w-100 h-100 bg-red-300 rounded-xl">
+        <h1 className="p-5 m-auto w-100 h-30 text-center text-2xl content-center font-bold bg-red-400 rounded-t-xl">
+          Giros aqui!
+        </h1>
+      </div>
+    </div>
+    
+  )
+}
---
+++ src/app/userpokes/page.tsx
@@ -0,0 +1,15 @@
+export default function UserPokes() {
+    return(
+    <div className=" h-[60vh] bg-white content-center">
+      <div className="m-auto w-100 h-100 bg-red-300 rounded-xl">
+        <h1 className="p-5 m-auto w-100 h-30 text-center text-2xl content-center font-bold bg-red-400 rounded-t-xl">
+          Pokemóns do usuário!
+        </h1>
+        <h2 className="p-5 m-auto h-40 w-100 text-xl text-center content-center">
+          Aqui você pode encontrar informações sobre os Pokémons que você colecionar.
+        </h2>
+      </div>
+    </div>
+    
+  )
+}
